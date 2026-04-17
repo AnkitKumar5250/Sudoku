8afe133db489b193c54c71d18d045b1366792ba0
TheAk5250 <kumara1@bxscience.edu>
2026-04-17 08:10:47 -0400

    Added time checker.

--- Main.java
+++ Main.java
@@ -1,7 +1,21 @@
 public class Main {
   public static void main(String[] args) {
     SudokuGenerator generator = new SudokuGenerator();
-    while (!SudokuGenerator.isValid(generator.board())) generator.generate();
+    
+    int iterations = 1000;
+    long totalTime = 0;
+    for (int index = 0; index < iterations; index++) {
+      long startTime = System.nanoTime();
+      generator.generate();
+      long endTIme = System.nanoTime();
+
+      totalTime += (endTIme - startTime);
+    }
+
     generator.printBoard();
+    double averageTime = totalTime * 1.0 / iterations;
+    double millis = averageTime / 1000000;
+
+    System.out.print("AVERAGE TIME: " + millis);
   }
 }
